d6327949202b18a27d12a83539a71f6d4e8e2ff2
Larissa <lari.ayumim@gmail.com>
2025-10-03 14:14:22 -0300

    Atualização dos arquivos

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+//declarando as constantes
+const DATA_NASCIMENTO = '01/01/1995';
+const CPF = '252.433.368-04';
+const RG_SP = '29.365.924-6';
+
+//exibindo no console
+console.log(DATA_NASCIMENTO);
+console.log(CPF);
+console.log(RG_SP);
